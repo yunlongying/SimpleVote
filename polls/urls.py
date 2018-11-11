c8b5b414ae316704f4ff6b16569d6bdeaba33be2
ying.yunlong <ying.yunlong@zte.com.cn>
2018-11-11 18:25:44 +0800

    Delete unused pycache files
    Signed-off-by: ying.yunlong <ying.yunlong@zte.com.cn>

--- polls/urls.py
+++ polls/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path, include
+from django.urls import path
+
+from polls import views
 
 urlpatterns = [
-    # path('polls/', include()),
+    path('list/', views.list_polls),
+    path('add/', views.add_polls),
+    path('get/<int:polls_id>/', views.get_polls),
+    path('update/<int:polls_id>/', views.update_polls),
+    path('delete/<int:polls_id>/', views.delete_polls),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,29 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def list_polls(request):
+    resp = 'List all questions successfully!'
+    return HttpResponse(content=resp, status=200)
+
+
+def add_polls(request):
+    resp = 'Add question(%s) successfully!' % 11
+    return HttpResponse(content=resp, status=200)
+
+
+def get_polls(request, polls_id):
+    resp = 'Get question(%s) successfully!' % polls_id
+    return HttpResponse(content=resp, status=200)
+
+
+def update_polls(request, polls_id):
+    resp = 'Update question(%s) successfully!' % polls_id
+    return HttpResponse(content=resp, status=200)
+
+
+def delete_polls(request, polls_id):
+    resp = 'Delete question(%s) successfully!' % polls_id
+    return HttpResponse(content=resp, status=200)

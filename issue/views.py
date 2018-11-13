15a1e87647f329a0dcac6943c64128913044230e
yunlongying <fengyuan1210@163.com>
2018-11-13 22:52:56 +0800

    Add issue_detail template and and issue function
    Signed-off-by: yunlongying <fengyuan1210@163.com>

--- issue/views.py
+++ issue/views.py
@@ -1,3 +1,7 @@
+import json
+import uuid
+from datetime import datetime
+
 from django.http import HttpResponse
 from django.shortcuts import render
 # from django.template import loader
@@ -17,18 +21,33 @@ def index(request):
 
 
 def list_issue(request):
+    issues = IssueModel.objects.all()
+    issue_list = []
+    for issue in issues:
+        issue_list.append({
+            'issue_id': issue.issue_id,
+            'issue_name': issue.issue_name,
+            'choice': issue.choice,
+            'create_data': str(issue.create_date)
+        })
     resp = 'List all questions successfully!'
-    return HttpResponse(content=resp, status=200)
+    return HttpResponse(content=json.dumps(issue_list), status=200)
 
 
 def add_issue(request):
-    resp = 'Add question(%s) successfully!' % 11
+    issue_id = uuid.uuid4()
+    data = json.loads(request.body)
+    issue_name = data.get('issue_name')
+    choice = data.get('choice')
+    IssueModel.objects.create(issue_id=issue_id, issue_name=issue_name, choice=choice, create_date=datetime.now())
+    resp = 'Add question(%s) successfully!' % issue_id
     return HttpResponse(content=resp, status=200)
 
 
 def get_issue(request, issue_id):
-    resp = 'Get question(%s) successfully!' % issue_id
-    return HttpResponse(content=resp, status=200)
+    question = IssueModel.objects.get(issue_id=issue_id)
+    context = {'question': question}
+    return render(request, 'issue/issue_detail.html', context)
 
 
 def update_issue(request, issue_id):
